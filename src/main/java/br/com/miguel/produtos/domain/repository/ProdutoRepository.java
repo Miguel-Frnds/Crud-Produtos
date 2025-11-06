1008f722b946e3dfad3964958cb86f6392343c54
Miguel-Frnds <miguel.pereiraf05@gmail.com>
2025-11-06 18:07:04 -0300

    feat: adicionar método para buscar produtos por nome parcial

--- src/main/java/br/com/miguel/produtos/domain/repository/ProdutoRepository.java
+++ src/main/java/br/com/miguel/produtos/domain/repository/ProdutoRepository.java
@@ -3,7 +3,9 @@ package br.com.miguel.produtos.domain.repository;
 import br.com.miguel.produtos.domain.entity.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface ProdutoRepository extends JpaRepository<Produto, UUID> {
+    List<Produto> findByNomeContainingIgnoreCase(String nome);
 }
