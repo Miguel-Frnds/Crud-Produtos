73ec6a3a1d6c6366105127a6b2323eeecd8c4d07
Miguel-Frnds <miguel.pereiraf05@gmail.com>
2025-11-06 15:38:00 -0300

    feat: criar classe produto

---
+++ src/main/java/br/com/miguel/produtos/domain/entity/Produto.java
@@ -0,0 +1,37 @@
+package br.com.miguel.produtos.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Table(name = "produto")
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Produto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    private String descricao;
+
+    @Column(name = "valor_unitario", nullable = false)
+    private Double preco;
+
+    @Column(nullable = false)
+    private Integer quantidade;
+
+    public Produto(String nome, String descricao, Double preco, Integer quantidade) {
+        this.nome = nome;
+        this.descricao = descricao;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+}
