7737e96763741924716816ad828a579fae1400c8
Miguel-Frnds <miguel.pereiraf05@gmail.com>
2025-11-06 16:17:37 -0300

    feat: criar estrutura inicial do controller para produto

---
+++ src/main/java/br/com/miguel/produtos/api/controller/ProdutoController.java
@@ -0,0 +1,48 @@
+package br.com.miguel.produtos.api.controller;
+
+import br.com.miguel.produtos.domain.entity.Produto;
+import br.com.miguel.produtos.domain.service.ProdutoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping(value = "/produtos")
+public class ProdutoController {
+
+    @Autowired
+    private ProdutoService produtoService;
+
+    @GetMapping
+    public ResponseEntity<List<Produto>> findAll(){
+        List<Produto> produtos = produtoService.findAll();
+        return ResponseEntity.ok().body(produtos);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Produto> findById(@PathVariable UUID id){
+        Produto produtoEncontrado = produtoService.findById(id);
+        return ResponseEntity.ok().body(produtoEncontrado);
+    }
+
+    @PostMapping
+    public ResponseEntity<Produto> save(@RequestBody Produto produto){
+        Produto produtoSalvo = produtoService.save(produto);
+        return ResponseEntity.ok().body(produtoSalvo);
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<Produto> update(@PathVariable UUID id, @RequestBody Produto produto){
+        Produto produtoAtualizado = produtoService.update(id, produto);
+        return ResponseEntity.ok().body(produtoAtualizado);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteById(@PathVariable UUID id){
+        produtoService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
