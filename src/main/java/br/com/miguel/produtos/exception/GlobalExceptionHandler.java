eda8b8d1a2a5456ffc0952bfc3a13ef7e6666bba
Miguel-Frnds <miguel.pereiraf05@gmail.com>
2025-11-07 19:08:56 -0300

    refactor: atualizar GlobalExceptionHandler para usar ErroResponse padronizado

--- src/main/java/br/com/miguel/produtos/exception/GlobalExceptionHandler.java
+++ src/main/java/br/com/miguel/produtos/exception/GlobalExceptionHandler.java
@@ -10,30 +10,30 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(ProdutoNotFoundException.class)
-    public ResponseEntity<String> produtoNotFoundHandler(ProdutoNotFoundException exception){
-        return ResponseEntity
-                .status(HttpStatus.NOT_FOUND)
-                .body(exception.getMessage());
+    public ResponseEntity<ErroResponse> produtoNotFoundHandler(ProdutoNotFoundException exception){
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        ErroResponse erro = new ErroResponse(exception.getMessage(), status.value());
+        return ResponseEntity.status(status).body(erro);
     }
 
     @ExceptionHandler(ProdutoSemNomeException.class)
-    public ResponseEntity<String> produtoSemNomeHandler(ProdutoSemNomeException exception){
-        return ResponseEntity
-                .status(HttpStatus.BAD_REQUEST)
-                .body(exception.getMessage());
+    public ResponseEntity<ErroResponse> produtoSemNomeHandler(ProdutoSemNomeException exception){
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        ErroResponse erro = new ErroResponse(exception.getMessage(), status.value());
+        return ResponseEntity.status(status).body(erro);
     }
 
     @ExceptionHandler(ValorInvalidoException.class)
-    public ResponseEntity<String> valorInvalidoHandler(ValorInvalidoException exception){
-        return ResponseEntity
-                .status(HttpStatus.BAD_REQUEST)
-                .body(exception.getMessage());
+    public ResponseEntity<ErroResponse> valorInvalidoHandler(ValorInvalidoException exception){
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        ErroResponse erro = new ErroResponse(exception.getMessage(), status.value());
+        return ResponseEntity.status(status).body(erro);
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> ExceptionGenericaHandler(Exception exception){
-        return ResponseEntity
-                .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body("Ocorreu um erro inesperado.");
+    public ResponseEntity<ErroResponse> ExceptionGenericaHandler(Exception exception){
+        HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
+        ErroResponse erro = new ErroResponse(exception.getMessage(), status.value());
+        return ResponseEntity.status(status).body(erro);
     }
 }
